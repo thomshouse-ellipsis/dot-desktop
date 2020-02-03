e6a9d3fc1f828d39560e5418273edcf8977cbceb
Thom Stricklin <thom@thomshouse.net>
2020-02-02 22:05:07 -0700

    Fix logic for installing/updating packages

---
+++ README.md
@@ -0,0 +1,12 @@
+# thomshouse-ellipsis/desktop
+Meta-package for installation of standard desktop dotfiles packages by [thomshouse][thomshouse].
+
+## Install
+Clone and symlink or install with [ellipsis][ellipsis]:
+
+```
+$ ellipsis install thomshouse-ellipsis/desktop
+```
+
+[ellipsis]: http://ellipsis.sh
+[thomshouse]: https://github.com/thomshouse
--- ellipsis.sh
+++ ellipsis.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/env bash
 
-pkg.install() {
-    packages=(
-        thomshouse-ellipsis/zsh
-    );
+packages=(
+    thomshouse-ellipsis/zsh
+);
     
-    for package in $packages; do
-        ellipsis.list_packages | grep "$ELLIPSIS_PACKAGES/$package" 2>&1 > /dev/null;
+pkg.install() {
+    for package in ${packages[*]}; do
+        echo "Checking for $(basename $package)...";
+        ellipsis.list_packages | grep "$ELLIPSIS_PACKAGES/$(basename $package)" 2>&1 > /dev/null;
         if [ $? -ne 0 ]; then
             $ELLIPSIS_PATH/bin/ellipsis install $package;
         else
-            $ELLIPSIS_PATH/bin/ellipsis pull $package;
+            $ELLIPSIS_PATH/bin/ellipsis pull $(basename $package);
         fi
     done
 }
