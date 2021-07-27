eaba09d37d61d296c85226ca2a9438376df87f80
Thom Stricklin <thomshouse@gmail.com>
2021-07-26 23:37:07 -0700

    Fix logic merging prerequisites and packages

--- ellipsis.sh
+++ ellipsis.sh
@@ -15,24 +15,19 @@ pkg.install() {
     # Determine which set of packages we'll install based on OS
     case "$(uname -rs)" in
         *WSL2)
-            sets=( wsl_prereqs packages )
-            ;;
-        *)
-            sets=( packages )
+            packages=("${wsl_prereqs[@]}" "${packages[@]}" )
             ;;
     esac
 
     # Loop through each set of packages and each package, installing or pulling latest
-    for set in ${sets[*]}; do
-        for package in ${set[*]}; do
-            echo "Checking for $(basename $package)...";
-            ellipsis.list_packages | grep "$ELLIPSIS_PACKAGES/$(basename $package)" 2>&1 > /dev/null;
-            if [ $? -ne 0 ]; then
-                $ELLIPSIS_PATH/bin/ellipsis install $package;
-            else
-                $ELLIPSIS_PATH/bin/ellipsis pull $(basename $package);
-            fi
-        done
+    for package in ${packages[*]}; do
+        echo "Checking for $(basename $package)...";
+        ellipsis.list_packages | grep "$ELLIPSIS_PACKAGES/$(basename $package)" 2>&1 > /dev/null;
+        if [ $? -ne 0 ]; then
+            $ELLIPSIS_PATH/bin/ellipsis install $package;
+        else
+            $ELLIPSIS_PATH/bin/ellipsis pull $(basename $package);
+        fi
     done
 }
 
