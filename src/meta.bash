ddeeaaefb0bc34b86cf70f51cc4a80354d051a01
thomshouse@gmail.com <thomshouse@gmail.com>
2022-05-04 17:01:26 -0700

    Remove pulling of existing ellipsis packages

--- src/meta.bash
+++ src/meta.bash
@@ -32,14 +32,12 @@ meta.install_packages() {
         DEBIAN_FRONTEND=noninteractive sudo apt-get update -y && sudo apt-get install -y ${apt_packages[@]}
     fi
 
-    # Loop through each set of packages and each package, installing or pulling latest
+    # Loop through each set of packages and each package, installing if it's not already installed
     for package in ${packages[*]}; do
         echo "Checking for $(basename $package)...";
         ellipsis.list_packages | grep "$ELLIPSIS_PACKAGES/$(basename $package)" 2>&1 > /dev/null;
         if [ $? -ne 0 ]; then
             $ELLIPSIS_PATH/bin/ellipsis install $package;
-        else
-            $ELLIPSIS_PATH/bin/ellipsis pull $(basename $package);
         fi
     done
 
